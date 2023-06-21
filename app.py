f34d77a489570ba51257bad3f867f562df3ea031
Chase Medford <chasemedford1@gmail.com>
2023-06-20 21:48:59 -0500

    Learning Python. - Functions

--- app.py
+++ app.py
@@ -344,3 +344,12 @@ def hello_func4(greeting, name='You'):
 
 
 print(hello_func4('Hi', name='Chase'))
+
+
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+student_info('Math', 'Art', name='John', age=22)
+
