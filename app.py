de0ab97ee9cdea4455aa764d272f41407a0bca1d
Chase Medford <chasemedford1@gmail.com>
2023-06-26 23:08:55 -0500

    Learning Python. - Functions - Args and Kwargs

--- app.py
+++ app.py
@@ -351,5 +351,9 @@ def student_info(*args, **kwargs):
     print(kwargs)
 
 
-student_info('Math', 'Art', name='John', age=22)
+# student_info('Math', 'Art', name='John', age=22)
 
+courses = ['Math', 'Art']
+info = {'name': 'John', 'age': 22}
+
+student_info(*courses, **info)
