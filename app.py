40c98e71fc52cefa7d7f4afb07d7748656fc2831
Chase Medford <chasemedford1@gmail.com>
2023-06-27 01:10:45 -0500

    Learning Python. - Functions - Args and Kwargs

--- app.py
+++ app.py
@@ -330,30 +330,49 @@
 #
 # hello_func2()
 
-def hello_func3():
-    return "Hello"
+# def hello_func3():
+#     return "Hello"
+#
+#
+# hello_func3()
+# print(hello_func3())
+# print(hello_func3().upper())
+#
+#
+# def hello_func4(greeting, name='You'):
+#     return '{}, {}'.format(greeting, name)
+#
+#
+# print(hello_func4('Hi', name='Chase'))
 
 
-hello_func3()
-print(hello_func3())
-print(hello_func3().upper())
-
-
-def hello_func4(greeting, name='You'):
-    return '{}, {}'.format(greeting, name)
-
-
-print(hello_func4('Hi', name='Chase'))
-
-
-def student_info(*args, **kwargs):
-    print(args)
-    print(kwargs)
+# def student_info(*args, **kwargs):
+#     print(args)
+#     print(kwargs)
 
 
 # student_info('Math', 'Art', name='John', age=22)
 
-courses = ['Math', 'Art']
-info = {'name': 'John', 'age': 22}
+# courses = ['Math', 'Art']
+# info = {'name': 'John', 'age': 22}
+#
+# student_info(*courses, **info)
 
-student_info(*courses, **info)
+
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+# # of days per month. First value placeholder for indexing purposes.
+def is_leap(year):
+    # Return true for leap years and false for non leap years.
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+def days_in_month(year, month):
+    # Return number of days in that month in that year.
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
+    if month == 2 and is_leap(year):
+        return 29
+    return month_days[month]
+
